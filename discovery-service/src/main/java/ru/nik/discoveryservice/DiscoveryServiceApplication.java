c23ac1a68e85d545314ed9569bd23760c8f07351
ujwegh <nik29200018@yandex.ru>
2021-11-04 13:03:30 +0300

    add start configs

--- discovery-service/src/main/java/ru/nik/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/ru/nik/discoveryservice/DiscoveryServiceApplication.java
@@ -2,7 +2,9 @@ package ru.nik.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServiceApplication {
 
