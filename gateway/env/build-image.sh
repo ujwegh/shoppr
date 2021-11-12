d9f00e28f3f28d5aacc02f1e84829fbb333c59fd
ujwegh <nik29200018@yandex.ru>
2021-11-12 10:33:03 +0300

    add jib build image

---
+++ gateway/env/build-image.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+display_usage() {
+  echo -e "\n This script build image and push it to registry."
+  echo -e "\nUSAGE:\n\n ${0} <tag> \n"
+}
+
+if [ $# -lt 1 ]; then
+  display_usage
+  exit 1
+fi
+
+WORK_DIR=$(
+  cd "$(dirname "$0")"
+  cd ..
+  pwd
+)
+
+REGISTRY_URL="127.0.0.1:5000"
+TAG_NAME=${1}
+SERVICE_NAME="gateway"
+echo "$WORK_DIR"
+
+cd "$WORK_DIR" || exit
+echo "$WORK_DIR"
+./gradlew clean buildDependents
+./gradlew jib -Pregistry_url=$REGISTRY_URL -Pimage_tag=$TAG_NAME -Djib.allowInsecureRegistries=true --stacktrace || true
+
+echo "IMAGE: ${REGISTRY_URL}/${SERVICE_NAME}:${TAG_NAME}"
