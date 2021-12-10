47ffcf60dc868ad6340bcb4e2c000d409c145f30
ujwegh <nik29200018@yandex.ru>
2021-12-10 15:01:49 +0300

    add resources deploy scripts

---
+++ startup-scripts/deploy-postgresql.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+DEPLOYMENT_NAME="db"
+NAMESPACE=${1}
+DEPLOYMENT_SUFFIX=${NAMESPACE:0:3}
+
+display_usage() {
+    echo -e "\n This script deploys postgresql database pod to specified k8s namespace."
+    echo -e "\nUSAGE:\n\n ${0} <namespace>"
+}
+
+if [ $# -lt 1 ]; then
+    display_usage
+    exit 1
+fi
+
+if [ $(helm repo list | grep https://charts.bitnami.com/bitnami | wc -l) == '0' ]
+then
+        echo '----------------------------------------------------------------------------'
+        echo 'adding missing helm repository with kafka chart'
+        helm repo add bitnami https://charts.bitnami.com/bitnami
+        helm repo update
+        echo '----------------------------------------------------------------------------'
+fi
+
+kubectl config set-context $NAMESPACE --namespace=$NAMESPACE --cluster=docker-desktop --user=docker-desktop || true
+kubectl config use-context $NAMESPACE
+
+helm delete ${DEPLOYMENT_NAME}-${DEPLOYMENT_SUFFIX} || true
+
+echo Deleting PVC: data-${DEPLOYMENT_NAME}-${DEPLOYMENT_SUFFIX}-postgresql-0
+kubectl delete pvc data-${DEPLOYMENT_NAME}-${DEPLOYMENT_SUFFIX}-postgresql-0 || true
+
+helm install \
+    ${DEPLOYMENT_NAME}-${DEPLOYMENT_SUFFIX} --create-namespace --namespace $NAMESPACE \
+    --set "name=${DEPLOYMENT_NAME}" \
+    -f values-postgresql.yaml \
+    bitnami/postgresql
+
+echo Wait ${DEPLOYMENT_NAME}-${DEPLOYMENT_SUFFIX}-postgresql-0
---
+++ startup-scripts/deploy-redis.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+DEPLOYMENT_NAME="cache"
+NAMESPACE=${1}
+DEPLOYMENT_SUFFIX=${NAMESPACE:0:3}
+
+display_usage() {
+    echo -e "\n This script deploys redis pod to specified k8s namespace."
+    echo -e "\nUSAGE:\n\n ${0} <namespace>"
+}
+
+if [ $# -lt 1 ]; then
+    display_usage
+    exit 1
+fi
+
+if [ $(helm repo list | grep https://charts.bitnami.com/bitnami | wc -l) == '0' ]
+then
+        echo '----------------------------------------------------------------------------'
+        echo 'adding missing helm repository with kafka chart'
+        helm repo add bitnami https://charts.bitnami.com/bitnami
+        helm repo update
+        echo '----------------------------------------------------------------------------'
+fi
+
+kubectl config set-context $NAMESPACE --namespace=$NAMESPACE --cluster=docker-desktop --user=docker-desktop || true
+kubectl config use-context $NAMESPACE
+
+helm delete ${DEPLOYMENT_NAME}-${DEPLOYMENT_SUFFIX} || true
+helm install ${DEPLOYMENT_NAME}-${DEPLOYMENT_SUFFIX} --create-namespace --namespace $NAMESPACE \
+    --set "name=${DEPLOYMENT_NAME}" \
+    -f values-redis.yaml \
+    bitnami/redis || true
+
+echo Wait ${DEPLOYMENT_NAME}-${DEPLOYMENT_SUFFIX}-redis-master-0
