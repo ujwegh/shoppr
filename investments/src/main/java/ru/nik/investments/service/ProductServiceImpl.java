bafd80d2f53bba6cb9685d9e20d7598187ee9b20
ujwegh <nik29200018@yandex.ru>
2022-02-23 16:04:27 +0300

    add mongo db

--- investments/src/main/java/ru/nik/investments/service/ProductServiceImpl.java
+++ investments/src/main/java/ru/nik/investments/service/ProductServiceImpl.java
@@ -1,24 +1,22 @@
 package ru.nik.investments.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.nik.investments.model.internal.Product;
 import ru.nik.investments.repository.InvestmentsRepository;
-import ru.nik.investments.repository.SomeOtherRepo;
 
 @Service
 public class ProductServiceImpl implements ProductService {
 
     private final InvestmentsRepository repository;
 
-    public ProductServiceImpl(@Autowired(required = true) InvestmentsRepository repository, @Autowired(required = true) SomeOtherRepo otherrepo) {
+    public ProductServiceImpl(@Autowired InvestmentsRepository repository) {
         this.repository = repository;
     }
 
-    @Override @Required
+    @Override
     public Mono<Product> findByUuid(String uuid) {
         return repository.findById(uuid);
     }
