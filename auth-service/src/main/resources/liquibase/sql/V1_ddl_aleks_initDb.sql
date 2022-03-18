b26a3ebb4f281c5bc053d1092fbd79e782410adc
ujwegh <nik29200018@gmail.com>
2022-03-18 17:13:32 +0300

    fix bd2

--- auth-service/src/main/resources/liquibase/sql/V1_ddl_aleks_initDb.sql
+++ auth-service/src/main/resources/liquibase/sql/V1_ddl_aleks_initDb.sql
@@ -8,8 +8,8 @@ CREATE TABLE ${auth.schemaName}.users
 (
     user_id    serial PRIMARY KEY,
     email     varchar(24) NOT NULL,
-    salt      varchar(24) NOT NULL, -- 16 bytes base64
-    hash      varchar(44) NOT NULL, -- 32 bytes base64
+    salt      varchar(32) NOT NULL, -- 16 bytes base64
+    hash      varchar(60) NOT NULL, -- 32 bytes base64
     secret_key varchar(32) NOT NULL
 );
 
